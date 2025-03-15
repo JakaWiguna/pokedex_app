a988119fe190aacedaebef159a11147cdb14d5a0
Jaka H Wiguna <jaka.hajar.wiguna@gmail.com>
2025-03-15 16:44:57 +0700

    test failed test

--- test/src/pokemon/presentation/bloc/pokemon_bloc_test.dart
+++ test/src/pokemon/presentation/bloc/pokemon_bloc_test.dart
@@ -204,7 +204,7 @@ void main() {
       const PokemonState(status: PokemonStateStatus.loading),
       const PokemonState(
         status: PokemonStateStatus.failure,
-        errorMessage: 'Unexpected Error',
+        errorMessage: 'Unexpected Erro',
       ),
     ],
     verify: (_) {
